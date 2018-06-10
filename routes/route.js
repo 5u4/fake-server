9e565a42c8a6057b1006b21747729c8f5265f239
Alex Wong <alexwongsenhung@gmail.com>
2018-06-10 08:01:51 -0700

    group apis

--- routes/route.js
+++ routes/route.js
@@ -1,4 +1,4 @@
-const sampleResponses = require('../apis/responses/sampleResponses.js');
+const sample = require('../apis/sample.js');
 
 /**
  * Routes are defined here.
@@ -7,10 +7,8 @@ const sampleResponses = require('../apis/responses/sampleResponses.js');
  */
 module.exports = {api: function (app) {
     /* a basic get api to get the app name */
-    app.get('/', (_, response) => response.send(sampleResponses.appName));
+    app.get('/', (request, response) => sample.index(request, response));
 
     /* a sample post request */
-    app.post('/', (request, response) => {
-        response.send(sampleResponses.create(request.body));
-    });
+    app.post('/', (request, response) => sample.create(request, response));
 }};
